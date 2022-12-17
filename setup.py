cd3af75c9f38f175f7764ad5a25850e704b0fb1a
Alexander Goryunov <alex.goryunov@gmail.com>
2022-12-17 17:28:35 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eval_lander',
+    version='0.1',
+    packages=['eval_lander'],
+    url='',
+    license='',
+    author='Alexander Goryunov',
+    author_email='alex.goryunov@gmail.com',
+    description=''
+)
