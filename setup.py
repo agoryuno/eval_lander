685792991a609c3b7f9502da7421184a5d2efac8
Alexander Goryunov <alex.goryunov@gmail.com>
2022-12-17 17:37:53 +0200

    Added required packages

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(
     license='',
     author='Alexander Goryunov',
     author_email='alex.goryunov@gmail.com',
-    description=''
+    description='',
+    requires_python='>=3.6',
+    install_requires=[
+        'gym',
+        'numpy',
+        'stable-baselines3',
+        'Box2D'
+    ]
 )
